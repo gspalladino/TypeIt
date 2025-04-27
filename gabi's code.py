bf9e2b5351c98259eb0746435b20264ca461a9b7
gspalladino <gpalladi@macalester.edu>
2025-04-27 02:00:04 -0500

    gui update + death screen
    I added pictures to all of the screens, and changed the fonts and moved some stuff around. I also added a death screen, it doesn't have the death image yet but i'll add that later. I put everything into a new python file called gabi's code just in case so it wouldn't mess up what you'd done already. I also made an images folder.
    
    For the pictures to work you have to download pillow into the typeIt repository. Not sure if it'll automatically be on urs cuz i added it to mine

---
+++ gabi's code.py
@@ -0,0 +1,115 @@
+import random
+import string
+import tkinter as tk
+import tkinter.messagebox as messagebox
+
+
+import PIL.Image as Image
+
+import PIL.ImageTk as ImageTk
+
+
+class TypeItGame:
+    def __init__(self):
+        self.mainWin = tk.Tk()
+        self.mainWin.geometry("350x450")
+        self.mainWin['bg'] = 'ivory'
+        self.startButton = tk.Button(bg = 'green', text = 'Start Game', padx = 10, pady = 10, fg='red', font="Comfortaa 40", command = self.openGameWin)
+        self.startButton.grid(row = 1, column =0)
+
+
+        photoOpen = Image.open("images/typeit3.png")
+
+
+        self.tracPic = ImageTk.PhotoImage(photoOpen)
+        imgLabel = tk.Label(self.mainWin, image=self.tracPic)
+        imgLabel.grid(row=0, column=0)
+
+    def openGameWin(self):
+        self.gameWin = tk.Toplevel(self.mainWin)
+
+        bgBopit = Image.open("images/bopitbg3.png")
+        self.bg_image = ImageTk.PhotoImage(bgBopit)
+
+
+        # self.promptCanvas = tk.Canvas(self.gameWin, bg = 'green', width="350", height="400")
+        self.promptCanvas = tk.Canvas(self.gameWin, width="350", height="400")
+        self.promptCanvas.grid(row = 0, column = 0)
+
+        self.promptCanvas.create_image(50, 0, image=self.bg_image, anchor='nw')
+
+        self.gameWin.bind("<KeyPress>", self.typeIt)
+        self.canvasText = self.promptCanvas.create_text(205, 138, text = 'type!', fill='red', font="Comfortaa 40" )
+        # self.promptEntry = tk.Entry(self.gameWin,bg = 'ivory', fg = 'black')
+        # self.promptEntry.grid(row=1,column=0)
+
+        pointtallyLabelLabel = tk.Label(self.gameWin, bg='ivory', fg='black', text="Points:")
+        pointtallyLabelLabel.grid(row=0, column=1, sticky="e")
+
+        self.pointTally = 0
+        self.pointTallyLabel = tk.Label(self.gameWin, bg='ivory', fg='black', text=str(self.pointTally))
+        self.pointTallyLabel.grid(row=0, column=2, sticky="w")
+    def typeIt(self,event):
+        keysym = event.keysym
+        if self.pointTally == 0 and keysym in string.ascii_lowercase:
+            self.pointTally += 1
+            self.labelName(random.choice(string.ascii_lowercase))
+            return print('game started')
+        while self.pointTally <= 100 and self.pointTally > 0:
+            # inpStr = "type " + self.prompt + ":"
+            # userInp = input(inpStr)
+            if keysym == self.prompt:
+                self.pointTally += 1
+                self.pointTallyLabel['text'] = self.pointTally
+                self.labelName(random.choice(string.ascii_lowercase))
+                return print('point total:', self.pointTally)
+            else:
+                print("you scored ", self.pointTally," points")
+                self.openDeathScreen()
+                return
+        while self.pointTally > 100 and self.pointTally <= 200:
+            prompt = random.choice(string.ascii_letters)
+            inpStr = 'type ' + prompt + ":"
+            userInp = input(inpStr)
+            if userInp == prompt:
+                self.pointTally += 1
+                print(self.pointTally)
+            else:
+                # print("you scored ", self.pointTally, " points")
+                self.openDeathScreen()
+                return
+    def labelName(self,prompt):
+        self.prompt = prompt
+        self.promptCanvas.itemconfig(self.canvasText, text=self.prompt)
+
+    def openDeathScreen(self):
+        print("Death screen should appear!")
+        self.deathScreen = tk.Toplevel(self.mainWin)
+        self.deathScreen.title("Game Over")
+        self.deathScreen.geometry("250x150")
+        self.deathScreen['bg'] = 'ivory'
+
+        gameOverLabel = tk.Label(self.deathScreen, text=f"Game Over!\nYou scored {self.pointTally} points.", bg='ivory', fg="red",
+                                 font="Comfortaa 20")
+        gameOverLabel.pack(pady=10)
+
+        playAgainButton = tk.Button(self.deathScreen, text="Play Again", command=self.playAgain, bg='green', fg='green')
+        playAgainButton.pack(pady=5)
+
+        quitButton = tk.Button(self.deathScreen, text="Quit", command=self.quitGame, bg='red', fg='black')
+        quitButton.pack(pady=5)
+
+    def playAgain(self):
+            self.gameWin.destroy()
+            self.deathScreen.destroy()
+            self.openGameWin()
+
+    def quitGame(self):
+            self.mainWin.destroy()
+
+    def go(self):
+        self.mainWin.mainloop()
+
+if __name__ == '__main__':
+    game = TypeItGame()
+    game.go()
